46f70a01adba95054d7161cefba2f58e657ebf8c
= <=>
2022-08-11 17:18:20 -0600

    Removed incorrect gitignore and added new one

--- target/debug/deps/glmath-132e47abf0568ac4.d
+++
@@ -1,6 +0,0 @@
-c:\Users\Bryce\Desktop\rust_projects\game_engine\glmath\target\debug\deps\glmath-132e47abf0568ac4.rmeta: src\main.rs src\tests.rs
-
-c:\Users\Bryce\Desktop\rust_projects\game_engine\glmath\target\debug\deps\glmath-132e47abf0568ac4.d: src\main.rs src\tests.rs
-
-src\main.rs:
-src\tests.rs:
--- target/debug/deps/glmath.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Bryce\Desktop\rust_projects\game_engine\glmath\target\debug\deps\glmath.exe: src\main.rs src\tests.rs
-
-C:\Users\Bryce\Desktop\rust_projects\game_engine\glmath\target\debug\deps\glmath.d: src\main.rs src\tests.rs
-
-src\main.rs:
-src\tests.rs:
